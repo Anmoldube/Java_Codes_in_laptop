225089c4bc17e705ff3c139aac260c6109778f9e
Anmol Dube <114559810+Anmoldube@users.noreply.github.com>
2024-12-08 13:44:06 +0530

    Add files via upload

---
+++ Abstraction/Son.java
@@ -0,0 +1,12 @@
+// package  Parent;
+public class Son extends  Parent {
+    @Override
+    public void  Career(String Pro) {
+        System.out.println("I am a Software Engineer" + Pro);
+    }
+    @Override
+    public void Partner(String Name) {
+        System.out.println("My partner is " + Name);
+    }
+
+}
---
+++ Abstraction/main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) {
+        Son s = new Son();
+        s.Career("Java Developer");
+        s.Partner("Astha web anmol");
+    }
+}
